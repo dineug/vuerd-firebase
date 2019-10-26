7afd7eedcceac38e45441149ea4604d60d81f665
dineug <dineug2@gmail.com>
2019-10-26 22:10:43 +0900

    add: lib

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <v-app class="app">
     <router-view />
-  </div>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
 
-#nav {
-  padding: 30px;
+@Component
+export default class App extends Vue {}
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style>
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import "./plugins/firebase";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./plugins/fragment";
 
 Vue.config.productionTip = false;
 
