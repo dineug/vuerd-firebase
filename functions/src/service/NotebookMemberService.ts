161a193ad6ea545450432d1620724654aaf4a62a
dineug <dineug2@gmail.com>
2019-12-16 23:12:20 +0900

    mod: functions js -> ts migration

--- functions/src/service/NotebookMemberService.ts
+++ functions/src/service/NotebookMemberService.ts
@@ -1,20 +1,22 @@
-const { functions, db } = require("../plugins/firebase");
-const {
+import { functions, db } from "../plugins/firebase";
+import {
   getNotificationColRef,
   getUsersDocRef,
   getNotebooksDocRef,
   getMembersDocRef
-} = require("../plugins/util");
-const moment = require("moment");
+} from "../plugins/util";
+import { Member, Notebook } from "./NotebookModel";
+import { User } from "./UserModel";
+import * as moment from "moment";
 
-exports.createNotebookMember = functions.firestore
+export const createNotebookMember = functions.firestore
   .document("notebooks/{notebookId}/members/{memberId}")
   .onCreate(async (snapshot, context) => {
-    const newMember = snapshot.data();
+    const newMember = snapshot.data() as Member;
     if (newMember.status === "invitation") {
       const batch = db.batch();
       const fromDoc = await getUsersDocRef(newMember.fromId).get();
-      const fromData = fromDoc.data();
+      const fromData = fromDoc.data() as User;
       batch.set(getNotificationColRef(context.params.memberId).doc(), {
         message: `${fromData.email} has invited you`,
         action: "invitation",
@@ -23,7 +25,7 @@ exports.createNotebookMember = functions.firestore
         createdAt: moment().unix()
       });
       const toDoc = await getUsersDocRef(context.params.memberId).get();
-      const toData = toDoc.data();
+      const toData = toDoc.data() as User;
       batch.update(toDoc.ref, {
         notification: toData.notification + 1
       });
@@ -31,11 +33,11 @@ exports.createNotebookMember = functions.firestore
     }
   });
 
-exports.updateNotebookMember = functions.firestore
+export const updateNotebookMember = functions.firestore
   .document("notebooks/{notebookId}/members/{memberId}")
   .onUpdate(async (change, context) => {
-    const afterMember = change.after.data();
-    const beforeMember = change.before.data();
+    const afterMember = change.after.data() as Member;
+    const beforeMember = change.before.data() as Member;
     if (
       beforeMember.status === "invitation" &&
       afterMember.status === "accept"
@@ -43,7 +45,7 @@ exports.updateNotebookMember = functions.firestore
       const notebookDoc = await getNotebooksDocRef(
         context.params.notebookId
       ).get();
-      const notebook = notebookDoc.data();
+      const notebook = notebookDoc.data() as Notebook;
       if (notebook.members.indexOf(context.params.memberId) === -1) {
         const members = notebook.members;
         const roles = notebook.roles;
@@ -59,7 +61,7 @@ exports.updateNotebookMember = functions.firestore
       const notebookDoc = await getNotebooksDocRef(
         context.params.notebookId
       ).get();
-      const notebook = notebookDoc.data();
+      const notebook = notebookDoc.data() as Notebook;
       const roles = notebook.roles;
       roles[context.params.memberId] = afterMember.role;
       let isOwner = false;
@@ -84,17 +86,17 @@ exports.updateNotebookMember = functions.firestore
     }
   });
 
-exports.deleteNotebookMember = functions.firestore
+export const deleteNotebookMember = functions.firestore
   .document("notebooks/{notebookId}/members/{memberId}")
   .onDelete(async (snapshot, context) => {
-    const member = snapshot.data();
+    const member = snapshot.data() as Member;
     if (member.status === "accept") {
       const notebookDoc = await getNotebooksDocRef(
         context.params.notebookId
       ).get();
       if (notebookDoc.exists) {
         const batch = db.batch();
-        const notebook = notebookDoc.data();
+        const notebook = notebookDoc.data() as Notebook;
         const members = notebook.members;
         const roles = notebook.roles;
         delete roles[context.params.memberId];
