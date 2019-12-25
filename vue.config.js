a45ce4a46c01bcdb7f2fe7a5a728df96a31e3dff
dineug <dineug2@gmail.com>
2019-12-25 23:27:53 +0900

    mod: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  productionSourceMap: false,
   css: {
     loaderOptions: {
       sass: {
@@ -7,10 +8,15 @@ module.exports = {
           @import "@/scss/size.scss";
         `
       }
+    },
+    extract: {
+      filename: "css/[name].css?hash=[chunkhash:8]",
+      chunkFilename: "css/[name].css?hash=[chunkhash:8]"
     }
   },
   chainWebpack(config) {
-    config.output.chunkFilename("js/[name].js?hash=[chunkhash]");
+    config.output.filename("js/[name].js?hash=[hash:8]");
+    config.output.chunkFilename("js/[name].js?hash=[chunkhash:8]");
     config.plugins.delete("prefetch");
   }
 };
